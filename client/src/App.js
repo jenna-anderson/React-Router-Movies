c958eb002cee70d3007d18b2f4bc8c7f69138fae
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-05-17 15:47:38 -0600

    added link functionality

--- client/src/App.js
+++ client/src/App.js
@@ -33,8 +33,8 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <Route path='/movies'>
-        <MovieList movie={movieList} />
+      <Route exact path='/'>
+        <MovieList movies={movieList} />
       </Route>
 
       <Route path='/movies/:movieID'>
